e5eb5a2434e4df1736bda376adac16f9194316c1
sdemirjian <sdemirjian@fwparker.org>
2017-02-10 14:27:34 -0600

    finished function problems

--- ch03_assignment.py
+++ ch03_assignment.py
@@ -7,6 +7,15 @@ The functions are already contained in the files.  Your job is to use them to en
 #1 Decrypt this message using imports from the decode.py and encryption_key.py.  Make the result print in a friendly format that is easy for the user to understand. (10pt)
 encrypted_message = "¿®ªªÈÙ®ÏT¤ÕEÓ¹âeCíÉÁÏº¢¡i¸ºÇ¿"
 
-#2 Encrypt your name and print the encrypted result.  Make the result print in a friendly format that is easy for the user to understand. (5pt)
+import decode
+import encryption_key
 
+print("The encrypted message is:", decode.decode(encryption_key.key,encrypted_message))
+print()
+#2 Encrypt your name and print the encrypted result.  Make the result print in a friendly format that is easy for the user to understand. (5pt)
+import encode
+encrypted_name = encode.encode(encryption_key.key, "Simon")
+print("My encrypted name is:", encrypted_name)
+print()
 #3 Decrypt the encrypted code from part 2 to ensure that it worked properly and print the result.  Make the result print in a friendly format that is easy for the user to understand. (5pt)
+print("My decoded name is:", decode.decode(encryption_key.key, encrypted_name))
